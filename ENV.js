5df2a70f2f9b15ee0f81eb088ac021f6dece2fbc
Turisap <kirill-shakirov@mail.ru>
2017-12-23 15:56:14 +0700

    ENV file

---
+++ ENV.js
@@ -0,0 +1,11 @@
+/**
+ * Created by HP on 23-Dec-17.
+ */
+const ENV = {
+    eventbriteAPI : {
+        OAuthToken : 'ESEQ74FBDHKEOYMZCR7G',
+        clientSecret : 'WTQ2SVTAGAUSOWK4B5THT345XCSFAGBFV2VVO2LSKYXF2UYM2I'
+    },
+};
+
+export default ENV;
