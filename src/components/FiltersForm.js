2b39041c60b4ee87e2747af0348fea8145596cda
Turisap <kirill-shakirov@mail.ru>
2017-12-24 18:51:20 +0700

    Got form with categories

--- src/components/FiltersForm.js
+++ src/components/FiltersForm.js
@@ -23,7 +23,6 @@ import {
 class FiltersForm extends React.Component {
 
     filters = {
-        categories : this.props.categories,
         types      : ['class', 'seminar', 'seminar', 'expo', 'party', 'conference',
             'networking', 'rally', 'game', 'retreat', 'gala'],
         dates : ['today', 'tomorrow', 'this week', 'this friday', 'this weekend', 'next week', 'this month'],
@@ -34,17 +33,17 @@ class FiltersForm extends React.Component {
         this.props.getCategories();
     }
 
+
     getFilters() {
         const html = [];
         Object.keys(this.filters).forEach(key => {
             html.push(
-                <div>
-                    <div>{key}</div>
-                    {this.filters[key].map(filter => {
-                       return  <div>{filter}</div>
+                <div className="filterForm__filter" key={key}>
+                    <div className="filterForm__group">{key}</div>
+                    {this.filters[key].map((filter, i) => {
+                       return  <div className="filterForm__control" key={i}>{filter}</div>
                     })}
                 </div>
-
             )
         });
         return html;
@@ -53,13 +52,17 @@ class FiltersForm extends React.Component {
     render(){
         return (
             <div id="filterForm">
-                <div className="filterForm__container">
-                    {this.getFilters()}
+                <div className="filterForm__filter">
+                    <div className="filterForm__group">Categories</div>
+                    {this.props.categories.map((category, i) => {
+                        return  <div className="filterForm__control" key={i}>{category.name}</div>
+                    })}
                 </div>
+                {this.getFilters()}
             </div>
         )
     }
-};
+}
 
 const mapStateToProps = state => ({
     categories : state.filters.allCategories,
