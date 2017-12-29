1d1e1ce494d3dc23a6af44e7bee25e1e102fc351
Turisap <kirill-shakirov@mail.ru>
2017-12-29 23:33:32 +0700

    Footer

--- src/app.js
+++ src/app.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import { Provider } from 'react-redux';
 import ConfigureStore from './store/ConfigureStore';
+import {SIGN_USER_IN} from './actions/types';
+
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 import 'antd/dist/antd.css';
-import {SIGN_USER_IN} from './actions/types';
 
 const store = ConfigureStore();
 
