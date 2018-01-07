417568c923a0a0d199ed7c76da41a299eb914dd5
Turisap <kirill-shakirov@mail.ru>
2018-01-08 02:11:40 +0700

    Additional test to the event selector

--- src/tests/selectors/events.test.js
+++ src/tests/selectors/events.test.js
@@ -9,3 +9,9 @@ test('should return an existing on a given id', () => {
     const event = findEventById(state, '25664286572');
     expect(event).toEqual([events[0]]);
 });
+
+test('should return an empty array if there is no an event with requested id', () => {
+    const state = {events : {events: events, filteredEvents: events}};
+    const event = findEventById(state, '25664286572900');
+    expect(event).toEqual([]);
+});
