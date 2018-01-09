65c118ddc5ea705be2bbbdadba4b62c6ffc3ba8b
Turisap <kirill-shakirov@mail.ru>
2018-01-09 23:02:16 +0700

    Server modifications for heroku

--- server/server/router.js
+++ server/server/router.js
@@ -5,15 +5,19 @@ const AuthController = require('../controllers/authController');
 const EventsController = require('../controllers/eventsController');
 const passportService = require('./services/passport');
 const passport = require('passport');
+const path = require('path');
 
 // this is ready to use auth middleware for authentication using jwt and mail
 const requireAuthMiddleware = passport.authenticate('jwt', {session : false});
 const requireSigninMiddleware = passport.authenticate('local', {session : false});
 
+// This part for heroku deployment (to serve index.html on '/' get request
+const publicPath = path.join(__dirname, '../..', 'public');
+
 
 module.exports = function (app) {
-    app.get('/', requireAuthMiddleware, function (req, res) {
-        res.send({message : 'Hi there'})
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(publicPath, 'index.html'));
     });
     app.post('/signup', AuthController.signUp);
     app.post('/signin', requireSigninMiddleware, AuthController.signIn);
