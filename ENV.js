08f2c48858b2ad2f9c34000add92fcf4f5babbbc
Kirill <kirill-shakirov@mail.ru>
2018-01-10 03:05:16 +0700

    Update ENV.js

--- ENV.js
+++ ENV.js
@@ -9,16 +9,16 @@
  * */
 const ENV = {
     eventbriteAPI : {
-        OAuthToken : 'Bearer ESEQ74FBDHKEOYMZCR7G',
-        clientSecret : 'WTQ2SVTAGAUSOWK4B5THT345XCSFAGBFV2VVO2LSKYXF2UYM2I',
+        OAuthToken : 'Bearer YOUR EVENTBRITE TOKEN',
+        clientSecret : 'YOUR EVENTBRITE CLIENT SECRET',
         rootURL : 'https://www.eventbriteapi.com/v3',
     },
     googleAPI : {
         rootURL : 'https://maps.googleapis.com/maps/api/geocode/json',
-        keyAPI  : 'AIzaSyDw5fwsSljNsyfchlULrBM4g984oGq8onY',
+        keyAPI  : 'YOUR API KEY FOR GOOGLE APPLICATION',
     },
     backendServer : {
-        rootUrl : 'https://event-finder-turisap.herokuapp.com'
+        rootUrl : 'URL TO YOUR APPLICATION'
     }
 };
 
